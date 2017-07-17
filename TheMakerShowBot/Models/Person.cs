8f4a901135193536a710fb06bf6c4f4dfa6a607c
Nick Landry <nick@mobility42.com>
2017-07-16 20:12:15 -0400

    Moved maker data assembly code to new MakerDataService class. Aslo added skeleton for Person data model to be used in Hero Cards

---
+++ TheMakerShowBot/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheMakerShowBot.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
